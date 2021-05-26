8ca855414996db0eb56b21f31390a46321e62c58
JamesArmstrong92 <jarmst23@gmail.com>
2021-05-25 23:19:41 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//background colour manipulator
+
+function colourChanger() {
+
+  let colour = document.getElementById("colour").value;
+
+  document.getElementById("colour-box").style.backgroundColor = colour
+
+  
+}
+
+//Box size manipulator
+
+function boxChanger() {
+
+  let size = document.getElementById("size").value;
+
+  document.getElementById("colour-box").style.padding = size
+
+
+}
